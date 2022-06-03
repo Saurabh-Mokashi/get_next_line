796bcb9b7cca22566f417f62de57a84a8ef629da
Saurabh Mokashi <smokashi@lab3r6s5.42abudhabi.ae>
2022-06-03 19:35:21 +0400

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: smokashi <smokashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:41:28 by smokashi          #+#    #+#             */
-/*   Updated: 2022/06/02 17:42:11 by smokashi         ###   ########.fr       */
+/*   Updated: 2022/06/03 18:41:07 by smokashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*buffalloc(int fd, char *s)
+{
+	char	*ret;
+
+	ret = malloc (BUFFER_SIZE * (sizeof(char)));
+	read(fd, ret, sz);
+	ret[sz] = NULL;
+	return (ret);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*buf;
+	char		*ret;
+
+	if (fd < 0)
+		return (NULL);
+	buf = buffalloc(fd, buf);
+
+	return (ret);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: smokashi <smokashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 18:00:31 by smokashi          #+#    #+#             */
-/*   Updated: 2022/06/02 19:40:39 by smokashi         ###   ########.fr       */
+/*   Updated: 2022/06/03 16:08:04 by smokashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 
 int main(void)
 {
-	char *c = (char *) calloc(100, sizeof(char));
+	printf("the buffer size is %d",BUFFER_SIZE);
+	char *c = (char *) malloc(7 * sizeof(char));
 	int fd=open("temp.txt",O_RDWR | O_CREAT);
-	int sz = read(fd,c,9);
-	c[10]='\0';
+	int sz = read(fd,c,6);
+	//c[18]='\0';
 	printf("%d is the amount of bytes read:\n",sz);
 	printf("%s is the string of bytes read:\n",c);
 	return (0);
